43cbb1b7beafd4ddb6e3209363a94f9c9a883b17
carlosevasquezg <carlosevasquezg@outlook.com>
2019-01-17 12:19:07 -0500

    Update FirstRun.R

--- FirstRun.R
+++ FirstRun.R
@@ -3,5 +3,25 @@ library(tidyverse)
 mpg
 # puedo ejecutar ?mpg para ver toda la documentacion
 
+
+# 1 variacion con shape, alpha, size
 ggplot( data = mpg) +
-  geom_point(mapping = aes (x = displ, y = hwy))
+  geom_point(mapping = aes (x = displ, y = hwy, alpha = class), color = "blue")
+
+# 2 Facets
+ggplot( data = mpg) +
+  geom_point(mapping = aes (x = displ, y = hwy)) +
+  facet_wrap (~class, nrow = 3)
+
+# 3 Facets dos variables
+ggplot( data = mpg) +
+  geom_point(mapping = aes (x = displ, y = hwy)) +
+  facet_grid (drv ~ cyl)
+
+#4 variando con geom_smooth
+ggplot( data = mpg) +
+  geom_smooth(mapping = aes (x = displ, y = hwy))
+
+#5 variando con tipos de linea
+ggplot( data = mpg) +
+  geom_smooth(mapping = aes (x = displ, y = hwy, linetype = drv))
